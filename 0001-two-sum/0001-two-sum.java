feaea7498b1962bcfc024d8dfccb6766c22174b0
SUNNY MISHRA <sunnymishra.atf@gmail.com>
2026-07-21 13:18:01 +0530

    Time: 2 ms (99.29%), Space: 46.7 MB (90.46%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,7 +1,8 @@
 import java.util.HashMap;
 
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
+public class Solution {
+
+    public static int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
@@ -15,5 +16,14 @@ class Solution {
         }
 
         return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = twoSum(nums, target);
+
+        System.out.println("[" + result[0] + ", " + result[1] + "]");
     }
 }
