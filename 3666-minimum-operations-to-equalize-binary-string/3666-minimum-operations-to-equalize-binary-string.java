e5aa477e8b618646320f64fc59ef912512589564
SUNNY MISHRA <sunnymishra.atf@gmail.com>
2026-07-25 15:39:08 +0530

    Time: 573 ms (16.9%), Space: 116.2 MB (20.66%) - LeetHub

--- 3666-minimum-operations-to-equalize-binary-string/3666-minimum-operations-to-equalize-binary-string.java
+++ 3666-minimum-operations-to-equalize-binary-string/3666-minimum-operations-to-equalize-binary-string.java
@@ -1,41 +1,40 @@
 class Solution {
+
     public int minOperations(String s, int k) {
-        int zero = 0;
-        int len = s.length();
-
-        for (int i = 0; i < len; i++)
-            zero += ~s.charAt(i) & 1;
-
-        if (zero == 0)
-            return 0;
-
-        if (len == k)
-            return (zero == len ? 1 : -1);
-
-        int base = len - k;
-
-        int odd = Math.max(
-            (zero + k - 1) / k,
-            (len - zero + base - 1) / base
-        );
-
-        odd += ~odd & 1;
-
-        int even = Math.max(
-            (zero + k - 1) / k,
-            (zero + base - 1) / base
-        );
-
-        even += even & 1;
-
-        int res = Integer.MAX_VALUE;
-
-        if ((k & 1) == (zero & 1))
-            res = Math.min(res, odd);
-
-        if ((~zero & 1) == 1)
-            res = Math.min(res, even);
-
-        return res == Integer.MAX_VALUE ? -1 : res;
+        int n = s.length();
+        int m = 0;
+        int[] dist = new int[n + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        List<TreeSet<Integer>> nodeSets = new ArrayList<>();
+        nodeSets.add(new TreeSet<>());
+        nodeSets.add(new TreeSet<>());
+        for (int i = 0; i <= n; i++) {
+            nodeSets.get(i % 2).add(i);
+            if (i < n && s.charAt(i) == '0') {
+                m++;
+            }
+        }
+        Queue<Integer> q = new ArrayDeque<>();
+        q.offer(m);
+        dist[m] = 0;
+        nodeSets.get(m % 2).remove(m);
+        while (!q.isEmpty()) {
+            m = q.poll();
+            int c1 = Math.max(k - n + m, 0);
+            int c2 = Math.min(m, k);
+            int lnode = m + k - 2 * c2;
+            int rnode = m + k - 2 * c1;
+            TreeSet<Integer> nodeSet = nodeSets.get(lnode % 2);
+            for (
+                Integer m2 = nodeSet.ceiling(lnode);
+                m2 != null && m2 <= rnode;
+                m2 = nodeSet.ceiling(lnode)
+            ) {
+                dist[m2] = dist[m] + 1;
+                q.offer(m2);
+                nodeSet.remove(m2);
+            }
+        }
+        return dist[0] == Integer.MAX_VALUE ? -1 : dist[0];
     }
 }
