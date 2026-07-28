8410689e5bc432c8e0b451a13ae09814e47d458e
SUNNY MISHRA <sunnymishra.atf@gmail.com>
2026-07-28 20:22:28 +0530

    Create 1482-HowManyNumbersAreSmallerThanTheCurrentNumber

---
+++ 1482-HowManyNumbersAreSmallerThanTheCurrentNumber/1482-HowManyNumbersAreSmallerThanTheCurrentNumber.java
@@ -0,0 +1,43 @@
+// Last updated: 28/07/2026, 20:22:27
+import java.util.Arrays;
+
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] count = new int[101];
+        int n = nums.length;
+        int[] result = new int[n];
+        
+       
+        for (int num : nums) {
+            count[num]++;
+        }
+        
+       
+        int runningSum = 0;
+        for (int i = 0; i < 101; i++) {
+            int freq = count[i];
+            count[i] = runningSum;
+            runningSum += freq;
+        }
+        
+      
+        for (int i = 0; i < n; i++) {
+            result[i] = count[nums[i]];
+        }
+        
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        
+        System.out.println(Arrays.toString(sol.smallerNumbersThanCurrent(new int[]{8, 1, 2, 2, 3}))); 
+      
+        
+        System.out.println(Arrays.toString(sol.smallerNumbersThanCurrent(new int[]{6, 5, 4, 8})));   
+    
+        
+        System.out.println(Arrays.toString(sol.smallerNumbersThanCurrent(new int[]{7, 7, 7, 7})));   
+     
+    }
+}
