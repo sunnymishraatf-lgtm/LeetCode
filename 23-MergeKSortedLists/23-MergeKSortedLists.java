73078833a04b8e8da519128264f88a838350f645
SUNNY MISHRA <sunnymishra.atf@gmail.com>
2026-07-28 20:22:43 +0530

    Create 23-MergeKSortedLists

---
+++ 23-MergeKSortedLists/23-MergeKSortedLists.java
@@ -0,0 +1,31 @@
+// Last updated: 28/07/2026, 20:22:41
+import java.util.PriorityQueue;
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) return null;
+        
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
+        
+        for (ListNode node : lists) {
+            if (node != null) {
+                pq.add(node);
+            }
+        }
+        
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+        
+        while (!pq.isEmpty()) {
+            ListNode node = pq.poll();
+            current.next = node;
+            current = current.next;
+            
+            if (node.next != null) {
+                pq.add(node.next);
+            }
+        }
+        
+        return dummy.next;
+    }
+}
